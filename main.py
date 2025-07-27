6185e2fcb2bfcfb0a43e84b465779fd05705bf7e
fire8811 <jensonjones2002@gmail.com>
2025-07-26 23:15:16 -0600

    add main.py with faissDB initialization

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+from faissDB import FaissDB
+
+""" RAG is mostly run here in this driver script """
+
+def main():
+    database = FaissDB()
+
+if __name__ == "__main__":
+    main()
+    print("\n===== PROGRAM FINISHED =====")
