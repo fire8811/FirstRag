5d060cd215fce2bf326c493e017337f6af2a52b9
fire8811 <jensonjones2002@gmail.com>
2025-07-26 22:52:30 -0600

    Add methods for initializing the faiss database

---
+++ faissDB.py
@@ -0,0 +1,44 @@
+import faiss
+import json
+import numpy as np
+import sys
+
+"""class for storing and handling FAISS database"""
+
+class FaissDB(self):
+    def __init__(self):
+        db = self.__initDB()
+
+    def __initDB(self):
+        vectors = self.__getVectors()
+        
+        d = len(vectors[0])
+        index = faiss.IndexFlatL2(d)
+        index.add(vectors)
+
+        #move index to GPU (overkill but why not)
+        resource = faiss.StandardGpuResources()
+        index = faiss.index_cpu_to_gpu(resource, 0, index)
+
+        print(f"--DB trained status:  {index.is_trained}")
+        print(f"--Vectors in DB: {index.ntotal}")
+        print(f"--DB CPU/GPU status: {type(index)}\n")
+
+        return index
+
+    def __getVectors(self):
+        with open("config.json", 'r') as c:
+            config_data = json.load(c)
+            embeddings_path = config_data["embedded-chunkpath"]
+         
+        with open(embeddings_path, 'r') as e:
+            chunk_data = json.load(c)
+
+        vector_list = []
+        for chunk in chunk_data:
+            vector_list.append(chunk["vector"])
+
+        return np.array(vector_list).astype('float32')
+
+
+        
