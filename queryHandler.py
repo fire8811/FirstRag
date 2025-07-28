1f2109781eb352a3470ddf67934447b8cb96ec12
fire8811 <jensonjones2002@gmail.com>
2025-07-27 21:33:12 -0600

    add method for calculating vector embedding of query

--- queryHandler.py
+++ queryHandler.py
@@ -2,7 +2,7 @@ import json
 import requests
 
 class QueryHandler:
-    def __init__(embedding_model, embedding_url, llm_url, llm_model):
+    def __init__(self, embedding_url, embedding_model, llm_url, llm_model):
         """
         embedding_url: the url for the embedding model (used for converting queries to vector embeddings)
         llm_url: the model of the LLM that receives and answers queries
@@ -11,4 +11,16 @@ class QueryHandler:
         self.embedding_model = embedding_model
         self.embedding_url = embedding_url
         self.llm_url = llm_url
-        self.llm_model = llm_model 
+        self.llm_model = llm_model
+
+    def get_query_vector(self, query):
+        """return vector embedding of the user's query"""
+
+        payload = {
+                'model': self.embedding_model,
+                'prompt': query
+                }
+
+        response = requests.post(self.embedding_url, json=payload)
+        out = response.json()
+        return out['embedding']
